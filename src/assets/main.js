08542044335093298b964ff1e2c15b5f49f5f432
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-08-29 18:56:38 -0300

    Ordenando los nombres

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,22 +1,16 @@
-const axios = require("axios");
+const fetch = require('node-fetch');
+
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCtTy-FTTdQWV1l7x9BHrtNw&part=snippet%2Cid&order=date&maxResults=9';
 
 const options = {
     method: 'GET',
-    url: 'https://youtube-v31.p.rapidapi.com/search',
-    params: {
-        channelId: 'UCtTy-FTTdQWV1l7x9BHrtNw',
-        part: 'snippet,id',
-        order: 'date',
-        maxResults: '9'
-    },
     headers: {
         'X-RapidAPI-Key': 'd925f9e514msh59f69670a0a2b49p1ce7aajsnabb107d97477',
         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
 };
 
-axios.request(options).then(function(response) {
-    console.log(response.data);
-}).catch(function(error) {
-    console.error(error);
-});
+fetch(API, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error('error:' + err));
